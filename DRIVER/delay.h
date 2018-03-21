62472bd332972456e2f098b9e673db11743f977c
zhangaiming <zhangam@efficien.cn>
2018-03-21 10:30:33 +0800

    Signed-off-by: zhangaiming <2320780008@qq.com>

--- DRIVER/delay.h
+++ DRIVER/delay.h
@@ -2,6 +2,10 @@
 #define __DELAY_H 			   
 #include "stm32f4xx.h"
 
+extern unsigned char Time_statr_flag;	//启动定时标志
+extern unsigned char Time_out_flag;		//超时标志
+extern unsigned int Time_wait_cnt;		//等待时间
+
 void delay_init(u8 SYSCLK);
 void delay_ms(u16 nms);
 void delay_us(u32 nus);
--- MAIN/main.c
+++ MAIN/main.c
@@ -8,15 +8,15 @@
 #include "main.h"
  int main()
  {	
-		SystemInit();
-		delay_init(168); 
-		NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4);//优先级设置  全为抢占式优先级
-		SysTick_Config(SystemCoreClock / 1000);			//uCOS时基1ms
-		ADC_Init_All();
-		DAC_Init_ALL();
-		Usart_Init_All();
+	SystemInit();
+	delay_init(168); 
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4);//优先级设置  全为抢占式优先级
+	SysTick_Config(SystemCoreClock / 1000);			//uCOS时基1ms
+	ADC_Init_All();
+	DAC_Init_ALL();
+	Usart_Init_All();
 	 LED_Init();
-		while(1);
+	 while(1);
  }
 
 
